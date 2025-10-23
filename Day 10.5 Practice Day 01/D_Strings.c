26beab8e236cf77e93d105cf89c12e98e1278e55
Md Monjur Bakth Mazumder <md.monjurmbm2001@gmail.com>
2025-10-23 19:29:44 +0600

    update

---
+++ Day 10.5 Practice Day 01/D_Strings.c
@@ -0,0 +1,22 @@
+// Problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/D
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a[11], b[11];
+
+    scanf("%s %s", &a, &b);
+
+    int alen = strlen(a);
+    int blen = strlen(b);
+
+    printf("%d %d\n", alen, blen);
+
+    printf("%s%s\n", a,b);
+
+    char tmp = a[0];
+    a[0] = b[0];
+    b[0] = tmp;
+    printf("%s %s", a, b);
+}
---
+++ Day 11.5 Practice Day 02/frequency_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int fre[6] = {0};
+
+    for (int i = 0; i < n; i++)
+    {
+        fre[arr[i]]++;
+    }
+
+    for (int i = 0; i < 6; i++)
+    {
+        printf("%d -> %d\n", i, fre[i]);
+    }
+
+    return 0;
+}
